ae86551fbd7dfed2b1930b2ff9e24e0c5c8aca2b
AndreasIT <32296603+AndreasIT@users.noreply.github.com>
2019-01-07 09:43:27 +0100

    Controller tilføjet, med 2 get metoder. En til alle afrejser og en til en bestemt med Id(opgave 1 og 2).

---
+++ Airport/Controllers/AirportController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Airport.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Airport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirportController : ControllerBase
+    {
+        private readonly AirportContext _context;
+
+        public AirportController(AirportContext context)
+        {
+            _context = context;
+
+            if (_context.flightItems.Count() == 0)
+            {
+                _context.flightItems.Add(new FlightItems { AircraftType = "Plane1" });
+                _context.SaveChanges();
+            }
+        }
+//Get all
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FlightItems>>> GetFlightItems()
+        {
+            return await _context.flightItems.ToListAsync();
+        }
+//Get With id
+        [HttpGet("{FlightId}")]
+        public async Task<ActionResult<FlightItems>> GetFlightItem(int FlightId)
+        {
+            var FlightItem = await _context.flightItems.FindAsync(FlightId);
+
+            if (FlightItem == null)
+            {
+                return NotFound();
+            }
+
+            return FlightItem;
+        }
+    }
+}
